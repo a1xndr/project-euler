e62d6f6e1f5539777edc0b4632529fe733433a0e
Alexander Oleinik <alxndr@solo.vrmnet>
2015-12-06 22:01:44 -0500

    Progress in 78

--- 078-Coin-Partitions/main.cpp
+++ 078-Coin-Partitions/main.cpp
@@ -25,15 +25,47 @@ using namespace std;
 
 const int target=1000;
 
-int coin_stacks(int n)
+int coin_stacks(int n, int max, int iter)
 {
-    if(n<=1)return 1; 
-    return 1+coin_stacks(n-1);
+    if(max==0)return 0;
+    if(n<=0){
+        //std::cout<<std::endl;
+        return 0;
+    }
+    //std::cout<<"---------"<<std::endl;
+    //std::cout<<"itr: "<<iter<<std::endl;
+    //std::cout<<"n  : "<<n<<std::endl;
+    //std::cout<<"max: "<<max<<std::endl;
+    int count=1;
+    int stacksize=max;
+    while(stacksize>0 && max>0)
+    {
+        for(int j=0; j<stacksize; j++)
+        {
+            //std::cout <<"o";
+        }
+            //std::cout <<"   ";
+        //std::cout<<std::endl;
+        stacksize--;
+        //std::cout<<"ss : "<<stacksize<<std::endl;
+        count+=coin_stacks(n-stacksize,stacksize, iter+1);
+    }
+    return count;
 }
 
 int main(int argc, char* argv[])
 {
-    std::cout<<coin_stacks(5)<<std::endl;
+    int i=1;
+    while(true){
+    int j=coin_stacks(i,i,0);
+    std::cout<<i<<": "<<j<<std::endl;
+    if(j%1000000==0)
+    {
+    std::cout<<j<<std::endl;
+    return 0;
+    }
+    i++;
+    }
     return 0;
 }
 
