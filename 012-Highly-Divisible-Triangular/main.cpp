8e6c7aa524e141fe5b75586db03f21cde25b5b10
Alexander Oleinik <alxndr@solo.vrmnet>
2016-11-18 15:30:29 -0500

    added 012

--- 012-Highly-Divisible-Triangular/main.cpp
+++ 012-Highly-Divisible-Triangular/main.cpp
@@ -18,34 +18,34 @@
 #include <stdlib.h>
 #include <iostream>
 #include <cmath>
-
+#include <map>
 long multiple=600851475143;
-int factor_list[10000000];
+
 long num_factors(long num)
 {
-    int facts=0;
-    for(int i=1; i<=num/2; i++)
+    if(map[num])return map[num];
+    for(int i=2; i<num/2 +1 ;i++)
     {
-        if(num%i==0)facts++;
+        if(num%i)
+        {
+
+        }
     }
-    return facts;
 }
 int main(int argc, char* argv[])
 {
-    long tri=1;
-    long step=2;
+    map<long, int> facts;
+    long tri=0;
+    long step=0;
     while(true)
     {
-        tri+=step;
+        map<long, bool> facts;
         step++;
-        std::cout<<std::endl<<"number:"<<tri<<std::endl;
-        int num_facts=num_factors(tri);
-        std::cout<<std::endl<<"number:"<<tri<<" has "<<num_facts <<" factors"<<std::endl;
-        if(num_facts>=500)
-        {
-            std::cout<<std::endl<<"number:"<<tri<<" has "<<num_facts <<" factors"<<std::endl;
-            break;
-        }
+        tri=(1.0+step)*(step/2.0);
+
+        std::cout << tri << " has " << num_factors(tri) << 
+                " factors." <<std::endl;
+        if(step==7)return 0;
     }
     
     return 0;
